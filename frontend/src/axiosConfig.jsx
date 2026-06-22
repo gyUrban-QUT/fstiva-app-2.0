17f383e40318b8af37f51c53dab626b528cf4dc6
gyUrban <129362600+gyUrban@users.noreply.github.com>
2026-06-22 17:31:05 +1000

    sync package managers

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // Automatically pulls from EC2 .env in production, or local .env in development
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
